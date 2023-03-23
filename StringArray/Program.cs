f690156b32dc83be161abba9a7598a5b841d68cb
ScarletStranger <maksimledenev6@gmail.com>
2023-03-23 21:28:18 +0300

    The program should be working but it isn't

--- StringArray/Program.cs
+++ StringArray/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] initialArray = new string[5] {"Serbia", "-164156", "git.exe", "1651.546", "conundrum"};
+string[] finalArray = new string[initialArray.Length];
+
+void SecondArrayWithIF(string[] initialArray, string[] finalArray)
+{
+    int count = 0;
+    for (int i = 0; i < initialArray.Length; i++)
+    {
+    if(initialArray[i].Length <= 3)
+        {
+        finalArray[count] = initialArray[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondArrayWithIF(initialArray, finalArray);
+PrintArray(finalArray);
